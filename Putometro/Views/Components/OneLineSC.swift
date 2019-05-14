a964f417e8a264404b90290955b9971805ced243
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-13 22:44:18 -0300

    onelineSG delegate + inital index = 0

--- Putometro/Views/Components/OneLineSC.swift
+++ Putometro/Views/Components/OneLineSC.swift
@@ -14,20 +14,69 @@ class OneLineSC: UIControl {
     private var selectorView: UIView?
     private var stackView: UIStackView?
     
+    weak var delegate: OneLineSGDelegate?
+    
     var textColor: UIColor = #colorLiteral(red: 0.4950222373, green: 0.4920834899, blue: 0.4972836971, alpha: 1)
     var selectorViewColor: UIColor = #colorLiteral(red: 0.1768031418, green: 0.1757590771, blue: 0.1776101589, alpha: 1)
     var selectorViewTextColor: UIColor = #colorLiteral(red: 0.1768031418, green: 0.1757590771, blue: 0.1776101589, alpha: 1)
+    var selectedIndex : Int = 0
+    
+    init(frame: CGRect, buttonTitles: [String]) {
+        self.buttonTitles = buttonTitles
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        setIndex(index: selectedIndex)
+        uptadeView()
+    }
+    
+//    Set Index of StackView
+    func setIndex(index: Int) {
+        let offset = frame.width/CGFloat(buttonTitles.count*2)/2
+        let selectorPosition = (frame.width/CGFloat(buttonTitles.count)*CGFloat(index))+offset
+//        Set initial index to 0
+        selectedIndex = index
+        delegate?.changeTo(index: index)
+        UIView.animate(withDuration: 0.3) {
+            guard let selectorView = self.selectorView else { return }
+            selectorView.frame.origin.x = selectorPosition
+        }
+    }
+    
+    @objc func buttonAction(sender: UIButton) {
+        for (buttonIndex, button) in buttons.enumerated() {
+            button.setTitleColor(textColor, for: .normal)
+            if button == sender {
+                setIndex(index: buttonIndex)
+                button.setTitleColor(selectorViewColor, for: .normal)
+            }
+        }
+    }
+}
+
+//Configuration Views
+extension OneLineSC {
+    private func uptadeView() {
+        createButton()
+        configStackView()
+        configSelectorView()
+    }
     
     private func configStackView() {
         stackView = UIStackView(arrangedSubviews: buttons)
-        
+//        Setup
         guard let stackView = stackView else { return }
         stackView.axis = .horizontal
         stackView.alignment = .fill
         stackView.distribution = .fillEqually
         addSubview(stackView)
-        
-//      Stack Constraints
+//        Constraints
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
         stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
@@ -36,23 +85,17 @@ class OneLineSC: UIControl {
     }
     
     private func configSelectorView() {
+//        Setup
         let selectorWidth = frame.width/CGFloat(buttonTitles.count*2)
-        selectorView = UIView(frame: CGRect(x: selectorWidth/2,
-                                            y: self.frame.height,
-                                            width: selectorWidth,
-                                            height: 1))
-        
+        selectorView = UIView(frame: CGRect(x: selectorWidth/2, y: self.frame.height,
+                                            width: selectorWidth, height: 1))
         guard let selectorView = selectorView else { return }
         selectorView.backgroundColor = selectorViewColor
         addSubview(selectorView)
-
     }
     
+//    Create Buttons to StackView
     private func createButton() {
-        buttons = [UIButton]()
-        buttons.removeAll()
-        subviews.forEach({$0.removeFromSuperview()})
-        
         for buttonTitle in buttonTitles {
             let button = UIButton(type: .system)
             button.setTitle(buttonTitle, for: .normal)
@@ -61,44 +104,5 @@ class OneLineSC: UIControl {
             buttons.append(button)
         }
         buttons[0].setTitleColor(selectorViewColor, for: .normal)
-    }
-    
-    @objc func buttonAction(sender: UIButton) {
-        for (buttonIndex, button) in buttons.enumerated() {
-            button.setTitleColor(textColor, for: .normal)
-            
-            if button == sender {
-                let offset = frame.width/CGFloat(buttonTitles.count*2)/2
-                let selectorPosition = (frame.width/CGFloat(buttonTitles.count)*CGFloat(buttonIndex))+offset
-                
-                UIView.animate(withDuration: 0.3) {
-                    guard let selectorView = self.selectorView else { return }
-                    selectorView.frame.origin.x = selectorPosition
-                }
-                
-                button.setTitleColor(selectorViewColor, for: .normal)
-            }
-        }
-    }
-    
-    private func uptadeView() {
-        createButton()
-        configStackView()
-        configSelectorView()
-    }
-
-    init(frame: CGRect, buttonTitles: [String]) {
-        self.buttonTitles = buttonTitles
-        
-        super.init(frame: frame)
-    }
-    
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        uptadeView()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
