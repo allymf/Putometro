0151ccfae298674f186974ab38fabafa9cc6ac91
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-14 21:43:45 -0300

    adding constraints + lazy var btn

--- Putometro/Views/ViewController.swift
+++ Putometro/Views/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    lazy var button: FillButton = {
+        let btn = FillButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0), title: "Done")
+        btn.isEnabled = true
+        btn.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let button = FillButton(frame: CGRect(x: view.center.x, y: 50,
-                                              width: view.frame.width/3,
-                                              height: view.frame.height/17),
-                                title: "Done")
         view.backgroundColor = #colorLiteral(red: 0.8822783828, green: 0.8824023604, blue: 0.8822392821, alpha: 1)
+        
         view.addSubview(button)
-        button.isEnabled = true
-        button.addTarget(self, action: #selector(buttonAction(_:)), for: .touchUpInside)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.widthAnchor.constraint(equalToConstant: view.frame.width/3).isActive = true
+        button.heightAnchor.constraint(equalToConstant: view.frame.height/17).isActive = true
+        button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
     }
     
     @objc func buttonAction(_ sender: UIButton) {
