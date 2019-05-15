582ca433fadc4e36e6fac22743aaeb0b6198d7a0
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-15 18:33:39 -0300

    add variables to teammateCell

--- Putometro/Views/Components/TeammateCell.swift
+++ Putometro/Views/Components/TeammateCell.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class TeammateCell: UITableViewCell {
+    lazy var circleView: CircleView = {
+        let circle = CircleView(frame: CGRect(origin: self.frame.origin, size: self.frame.size),
+                                color: UIColor.AppColors.red)
+        circle.translatesAutoresizingMaskIntoConstraints = false
+        return circle
+    }()
     
+    lazy var teammateCardView: TeammateCardView = {
+        let card = TeammateCardView(frame: CGRect(origin: self.frame.origin, size: self.frame.size),
+                                    name: "Beatriz Plutarco",
+                                    image: #imageLiteral(resourceName: "houcine-ncib-1579803-unsplash"))
+        card.translatesAutoresizingMaskIntoConstraints = false
+        return card
+    }()
 }
 
