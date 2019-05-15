a3cac07554b1e96321cfa5330e39102eaaa20b10
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-14 23:25:41 -0300

    create teammate card view

---
+++ Putometro/Views/Components/TeammateCardView.swift
@@ -0,0 +1,75 @@
+//
+//  TeammateCardView.swift
+//  Putometro
+//
+//  Created by Bia Plutarco on 14/05/19.
+//  Copyright © 2019 Abat. All rights reserved.
+//
+
+import UIKit
+
+class TeammateCardView: UIView {
+    var name: String
+    var image: UIImage
+    var cardViewColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    var shadowColor = #colorLiteral(red: 0.7262298465, green: 0.7219144702, blue: 0.7295480371, alpha: 1)
+    var shadorOffset = CGSize(width: 2, height: 16)
+    
+    lazy var teammateImage: UIImageView = {
+        let image = UIImageView()
+        image.image = self.image
+        image.layer.cornerRadius = self.frame.height/2.4
+        image.layer.shadowColor = self.shadowColor.cgColor
+        image.layer.shadowOffset = self.shadorOffset
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    lazy var teammateName: UILabel = {
+        let name = UILabel()
+        name.text = self.name
+        name.textColor = #colorLiteral(red: 0.1768031418, green: 0.1757590771, blue: 0.1776101589, alpha: 1)
+        name.translatesAutoresizingMaskIntoConstraints = false
+        return name
+    }()
+
+    init(frame: CGRect, name: String, image: UIImage) {
+        self.name = name
+        self.image = image
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        configCardView()
+        configTeammateImageConstraints()
+        configTeammateNameConstrains()
+    }
+    
+    private func configCardView() {
+        self.layer.backgroundColor = cardViewColor.cgColor
+        self.layer.cornerRadius = 8
+        self.layer.shadowColor = shadowColor.cgColor
+        self.layer.shadowOffset = shadorOffset
+        
+        self.addSubview(teammateName)
+        self.addSubview(teammateImage)
+    }
+    
+    private func configTeammateImageConstraints() {
+        teammateImage.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        teammateImage.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        teammateImage.widthAnchor.constraint(equalToConstant: self.frame.height/1.4).isActive = true
+        teammateImage.heightAnchor.constraint(equalToConstant: self.frame.height/1.4).isActive = true
+    }
+    
+    private func configTeammateNameConstrains() {
+        teammateName.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        teammateName.leftAnchor.constraint(equalTo: teammateImage.rightAnchor, constant: 10).isActive = true
+        teammateName.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 10).isActive = true
+    }
+}
--- Putometro/Views/ViewController.swift
+++ Putometro/Views/ViewController.swift
@@ -17,9 +17,10 @@ class ViewController: UIViewController {
         return btn
     }()
     
-    lazy var tableViewTest: UITableView = {
+    lazy var tableView: UITableView = {
         let table = UITableView()
-        table.backgroundColor = .gray
+        table.backgroundColor = #colorLiteral(red: 0.8822783828, green: 0.8824023604, blue: 0.8822392821, alpha: 1)
+        table.separatorStyle = .none
         table.translatesAutoresizingMaskIntoConstraints = false
         return table
     }()
@@ -28,7 +29,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.8822783828, green: 0.8824023604, blue: 0.8822392821, alpha: 1)
         view.addSubview(button)
-        view.addSubview(tableViewTest)
+        view.addSubview(tableView)
+//        Constraints
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.widthAnchor.constraint(equalToConstant: view.frame.width/3).isActive = true
+        button.heightAnchor.constraint(equalToConstant: view.frame.height/17).isActive = true
+        button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        
+        tableView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        tableView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -20).isActive = true
+        
+        let card = TeammateCardView(frame: CGRect(x: 100, y: 100, width: 200, height: 50), name: "beatriz plutarco", image: #imageLiteral(resourceName: "houcine-ncib-1579803-unsplash"))
+        view.addSubview(card)
+        view.bringSubviewToFront(card)
     }
     
     @objc func buttonAction(_ sender: UIButton) {
