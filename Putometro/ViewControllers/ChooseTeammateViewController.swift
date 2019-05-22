9c50d3c9f699a784f521d38951cbb92b63752666
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-22 09:08:19 -0300

    create choose teammate view controller file

---
+++ Putometro/ViewControllers/ChooseTeammateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChooseTeammateViewController.swift
+//  Putometro
+//
+//  Created by Bia Plutarco on 22/05/19.
+//  Copyright © 2019 Abat. All rights reserved.
+//
+
+import UIKit
+
+class ChooseTeammateViewController: UIViewController {
+    lazy var tableView: UITableView = {
+        return UITableView()
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.AppColors.ligthGray
+        
+        tableView.register(TeammateCell.self, forCellReuseIdentifier: "teammateCell")
+    }
+}
+
+extension ChooseTeammateViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+    }
+    
+    
+}
