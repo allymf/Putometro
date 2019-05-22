ce2d12f6bc334d94d36ed1d51d41373faa3784c3
Beatriz Plutarco <biaplutarco@gmail.com>
2019-05-22 09:33:19 -0300

    create teammateCell view model file

---
+++ Putometro/ViewModels/TeammateCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TeammateCellViewModel.swift
+//  Putometro
+//
+//  Created by Bia Plutarco on 22/05/19.
+//  Copyright © 2019 Abat. All rights reserved.
+//
+
+import UIKit
+
+struct TeammateCellViewModel {
+    <#fields#>
+}
